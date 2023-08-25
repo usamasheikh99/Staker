d870dc67c7860003e1297f8ebb0c7e08ea33b6c0
faizan zakir <dev.Faizan00@gmail.com>
2023-08-25 19:22:04 +0500

    hhh

--- src/screens/app/ReportScreen.js
+++ src/screens/app/ReportScreen.js
@@ -1,12 +1,46 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, TouchableOpacity, Image, } from 'react-native';
+import { Icons } from '../../constants';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function ReportScreen({ navigation }) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center',backgroundColor:'darkgray' }}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{ fontSize: 26, fontWeight: 'bold' , color:'black'}}>Details Screen</Text>
+        <View style={{ backgroundColor: Colors.background, flex: 1,}}>
+            <View>
+                <ImageBackground source={Icons.Homebg} resizeMode="cover" style={{ paddingHorizontal: 25, paddingTop: 68, paddingBottom: 30 }}>
+                    <View style={styles.header}>
+                        <TouchableOpacity>
+                            <Image source={Icons.left} style={styles.IconSize}></Image>
+                        </TouchableOpacity>
+
+                        <View>
+                            <Text style={styles.reporttxt}>Report</Text>
+                        </View>
+
+                    </View>
+
+                </ImageBackground>
+            </View>
+
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+
+    IconSize: {
+        height: 32,
+        width: 32
+    },
+
+    header: {
+        flexDirection: 'row',
+        
+    },
+
+    reporttxt: {
+        fontSize: 22,
+        color:Colors.white
+    }
+
+})
