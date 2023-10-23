96af025e8102737385afd3d02c2acbc46461a943
faizan zakir <dev.Faizan00@gmail.com>
2023-10-23 20:37:50 +0500

    postive modal test

--- src/component/Header.js
+++ src/component/Header.js
@@ -37,7 +37,7 @@ const Header = ({ navigation, auditType = false, OnOpenPopup, ScreenName = "", i
 
           {isReportPopup &&
             <Pressable style={styles.typebox} onPress={OnOpenPopup} activeOpacity={1}>
-              <Text >Type</Text>
+              <Text style={styles.type}>Type</Text>
             </Pressable>
           }
 
